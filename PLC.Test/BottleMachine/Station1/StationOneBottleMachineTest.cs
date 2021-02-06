b9204a8999f44be234a38268e183c8410c53e346
lais.lopes <lais.lopes@aluno.ifsp.edu.br>
2021-02-05 21:03:46 -0300

    Renames variables.

---
+++ PLC.Test/BottleMachine/Station1/StationOneBottleMachineTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using PLC.Domain;
+using Xunit;
+
+namespace PLC.Test.BottleMachine
+{
+    public class StationOneBottleMachineTest : IDisposable
+    {
+        private static ProgrammingLogicController _plc;
+
+        public StationOneBottleMachineTest()
+        {
+            _plc = new ProgrammingLogicController("127.0.0.1");
+            _plc.MapBitVariable("Sensor1", 2, 0, 1)
+                .MapBitVariable("Engine1", 4, 0, 1);
+        }
+
+        [Fact]
+        public void ShouldTurnOnEngineOneWhenSensorOneIsActive()
+        {
+            //Setup
+            SetEngineAs(false);
+
+            //Test
+            _plc.SetValueByTag("Sensor1", true);
+
+            //Assert
+            var motorStatus = Convert.ToBoolean(_plc.GetValueByTag("Engine1"));
+            Assert.True(motorStatus);
+
+        }
+
+        void SetEngineAs(bool status)
+        {
+            var currentStatus = Convert.ToBoolean(_plc.GetValueByTag("Engine1"));
+            if (currentStatus != status)
+                _plc.SetValueByTag("Sensor1", status);
+        }
+
+        public void Dispose()
+        {
+            _plc.Dispose();
+        }
+    }
+}
---
+++ PLC.Test/Engine/EngineTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using PLC.Domain;
+using Xunit;
+
+namespace PLC.Test.Engine
+{
+    public class EngineTest : IDisposable
+    {
+        private static ProgrammingLogicController _plc;
+
+        public EngineTest()
+        {
+            _plc = new ProgrammingLogicController("127.0.0.1");
+            _plc.MapBitVariable("StartButton", 2, 0, 0)
+                .MapBitVariable("Engine", 4, 0, 0);
+        }
+
+        [Fact]
+        public void ShouldTurnOffEngineWhenPressPowerButtonAndEngineIsTurnedOn()
+        {
+            //Setup
+            SetEngineAs(true);
+
+            //Test
+            _plc.SetValueByTag("StartButton", false);
+
+            //Assert
+            var engineStatus = Convert.ToBoolean(_plc.GetValueByTag("Engine"));
+            Assert.False(engineStatus);
+
+        }
+
+        [Fact]
+        public void ShouldTurnOnEngineWhenPressPowerButtonAndEngineIsTurnedOff()
+        {
+            //Setup
+            SetEngineAs(false);
+
+            //Test
+            _plc.SetValueByTag("StartButton", true);
+
+            //Assert
+            var engineStatus = Convert.ToBoolean(_plc.GetValueByTag("Engine"));
+            Assert.True(engineStatus);
+
+        }
+
+        void SetEngineAs(bool status)
+        {
+            var currentStatus = Convert.ToBoolean(_plc.GetValueByTag("Engine"));
+            if (currentStatus != status)
+                _plc.SetValueByTag("StartButton", status);
+        }
+
+        public void Dispose()
+        {
+            _plc.Dispose();
+        }
+    }
+}
