9da30af16d770f11540d53e4145c90f875416c0d
lais.lopes <lais.lopes@aluno.ifsp.edu.br>
2021-03-26 13:25:16 -0300

    Add comments.

--- PLCUnit/Domain/ProgrammableLogicController.cs
+++ PLCUnit/Domain/ProgrammableLogicController.cs
@@ -6,11 +6,21 @@ using System.Threading;
 
 namespace PLC.Domain
 {
+    /// <summary>
+    /// The <c>ProgrammableLogicController</c> is the main class of PLCUnit.
+    /// </summary>
+    /// <remarks>
+    /// This class estabilishes the communication with PLC, maps, writes and reads variables.
+    /// </remarks>
     public class ProgrammableLogicController : IDisposable
     {
         private readonly IPLCCommunication _plc;
         private readonly IDictionary<string, IType> _types;
 
+        /// <summary>
+        /// Opens the communication with PLC and initializes the <paramref name="_types"/> variables list.
+        /// </summary>
+        /// <param name="pLcCommunication">An unique name to the variable</param>
         public ProgrammableLogicController(IPLCCommunication pLcCommunication)
         {
             _types = new Dictionary<string, IType>();
@@ -19,6 +29,13 @@ namespace PLC.Domain
             Console.WriteLine("Conexão com CLP estabelecida com sucesso!");
         }
 
+        /// <summary>
+        /// Maps variables of Bit type from PLC to PLCUnit.
+        /// </summary>
+        /// <param name="tag">An unique name to the variable</param>
+        /// <param name="dbNumber">PLC Data Block number</param>
+        /// <param name="byteNumber">PLC Data Block Byte number</param>
+        /// <param name="bitNumber">PLC Data Block Bit number</param>
         public ProgrammableLogicController MapBitVariable(string tag,
                                                    int dbNumber,
                                                    int byteNumber,
@@ -28,6 +45,11 @@ namespace PLC.Domain
             _types.Add(tag, new Bit(_plc, dbNumber, byteNumber, bitNumber));
             return this;
         }
+
+        /// <summary>
+        /// Reads variable value from PLC.
+        /// </summary>
+        /// <param name="tag">An unique name to the variable</param>
         public string GetValueByTag(string tag)
         {
             var returnValue = _types[tag].Value;
@@ -35,6 +57,11 @@ namespace PLC.Domain
             return returnValue;
         }
 
+        /// <summary>
+        /// Writes variable value in PLC.
+        /// </summary>
+        /// <param name="tag">An unique name to the variable</param>
+        /// <param name="value">The variable value</param>
         public ProgrammableLogicController SetValueByTag(string tag, object value)
         {
             Console.WriteLine($"O dispositivo {tag} será {GetTextFromBoolean(value)}");
@@ -43,11 +70,18 @@ namespace PLC.Domain
             return this;
         }
 
+        /// <summary>
+        /// Converts a boolean value to text.
+        /// </summary>
+        /// <param name="value">The boolean variable value</param>
         private string GetTextFromBoolean(object value)
         {
             return (Convert.ToBoolean(value) ? "Ligado" : "Desligado");
         }
 
+        /// <summary>
+        /// Closes the communication with PLC.
+        /// </summary>
         public void Dispose()
         {
 
